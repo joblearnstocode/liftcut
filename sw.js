738ba12650ec268dabfb909cd3177578ec9ead98
joblearnstocode <joblearnstocode@gmail.com>
2025-12-24 09:21:24 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,18 @@
-const CACHE = "liftcut-v1";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json"];
+const CACHE = "liftcut-v2";
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./sw.js"];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE ? caches.delete(k) : null)))
+    )
+  );
 });
 
 self.addEventListener("fetch", (e) => {
